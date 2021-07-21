2bf64d90d211b6df293d4d5c79b0a6dbf6e3c901
Jean Marcel <jeanvanzella91@gmail.com>
2021-07-21 19:17:50 -0300

    tentando arrumar o git

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const getEmail = document.getElementById('email');
 const getSenha = document.getElementById('senha');
 const getButtom = document.querySelector('.botao');
+const getChekbox = document.querySelector('#agreement');
+const getButtomSubmit = document.querySelector('#submit-btn');
 
-getButtom.addEventListener('click', function(){
+getButtom.addEventListener('click', function () {
   if (getEmail.value === 'tryber@teste.com' && getSenha.value === '123456'){
     alert('Olá, Tryber!');
   }
   if (getEmail.value !== 'tryber@teste.com' && getSenha.value !== '123456'){
     alert('Login ou senha inválidos.');
   }
-})
+});
+
+getButtomSubmit.disabled = true;
+getChekbox.addEventListener('click', function () {
+  if (getChekbox.checked === true) {
+    getButtomSubmit.disabled = false;
+  } else if (getChekbox.checked === false) {
+    getButtomSubmit.disabled = true;
+  }
+});
