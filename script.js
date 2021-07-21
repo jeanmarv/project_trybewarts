df16d1f7b76602a5247f90ca4121a3eb9c655f42
Jean Marcel <jeanvanzella91@gmail.com>
2021-07-21 19:44:10 -0300

    lint java e css arrumado

--- script.js
+++ script.js
@@ -4,20 +4,24 @@ const getButtom = document.querySelector('.botao');
 const getChekbox = document.querySelector('#agreement');
 const getButtomSubmit = document.querySelector('#submit-btn');
 
-getButtom.addEventListener('click', function () {
-  if (getEmail.value === 'tryber@teste.com' && getSenha.value === '123456'){
+function validar() {
+  if (getEmail.value === 'tryber@teste.com' && getSenha.value === '123456') {
     alert('Olá, Tryber!');
   }
-  if (getEmail.value !== 'tryber@teste.com' && getSenha.value !== '123456'){
+  if (getEmail.value !== 'tryber@teste.com' && getSenha.value !== '123456') {
     alert('Login ou senha inválidos.');
   }
-});
+}
 
-getButtomSubmit.disabled = true;
-getChekbox.addEventListener('click', function () {
+getButtom.addEventListener('click', validar);
+
+function checar() {
   if (getChekbox.checked === true) {
     getButtomSubmit.disabled = false;
   } else if (getChekbox.checked === false) {
     getButtomSubmit.disabled = true;
   }
-});
+}
+
+getButtomSubmit.disabled = true;
+getChekbox.addEventListener('click', checar);
